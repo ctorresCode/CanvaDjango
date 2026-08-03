e740528cb85c0fa06d12ac4d6ea015718b8c1420
Carlos <crtorrescode@gmail.com>
2026-08-03 09:02:17 -0400

    feat: Creación de todas las apps del proyecto

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'soporte',
+    'evaluaciones',
+    'academico',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuarios.Usuario'
