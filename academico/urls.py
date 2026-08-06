7a01e1118e8820edc3ab0dee42c5481e3ff03dd1
Carlos <crtorrescode@gmail.com>
2026-08-06 16:25:34 -0400

    feat: creación de la funcionalidad de que un estudiante se puede inscribir en una clase y los cursos le salen en su men

--- academico/urls.py
+++ academico/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from academico.views import CursoCreateView, CursoDeleteView, CursoDetailView, CursoListView, CursoUpdateView
+from academico.views import CursoCreateView, CursoDeleteView, CursoDetailView, CursoListView, CursoUpdateView, InscribirCursoView, ListaCursoEstudianteView, MisCursosEstudianteView
 
 urlpatterns = [
     path('panel-maestro/academico/', CursoListView.as_view(), name='curso_list'),
@@ -8,4 +8,12 @@ urlpatterns = [
     path('panel-maestro/academico/crear_materia/', CursoCreateView.as_view(), name='curso_create'),
     path('panel-maestro/academico/editar_materia/<int:pk>/', CursoUpdateView.as_view(), name='curso_update'),
     path('panel-maestro/academico/eliminar_materia/<int:pk>/', CursoDeleteView.as_view(), name='curso_delete'),
+    
+    #desde el panel del estudiante, inscribirse a un curso
+    path('panel-estudiante/academico/', ListaCursoEstudianteView.as_view(), name='lista_curso_estudiante_inscripcion'),
+    path('panel-estudiante/academico/lista_cursos/inscribirse/<int:id_curso>/', InscribirCursoView.as_view(), name='inscribir_curso'),
+
+    #desde el panel del estudiante, ver los cursos en los que ya está inscrito
+    path('panel-estudiante/academico/mis-cursos/', MisCursosEstudianteView.as_view(), name='lista_curso_estudiante'),
+
 ]
--- academico/views.py
+++ academico/views.py
@@ -1,27 +1,31 @@
+from django.contrib import messages
+from django.http import JsonResponse, request
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
 
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView, View
 
 from academico import models
-from academico.models import Curso
+from academico.models import Curso, Inscripcion
+from usuarios.models import Estudiante
 
 # Create your views here.
 
-#Gestionar los cursos
+#Gestionar los cursos como maestro
 class CursoListView(ListView):
     model = Curso
     template_name = 'academi/curso_list.html'
     context_object_name = 'cursos'
 
-
+#detalles de los cursos como maestro
 class CursoDetailView(DetailView):
     model = Curso
     template_name = 'academi/curso_detail.html'
     context_object_name = 'curso'
 
-
+#creación de cursos como maestro
 class CursoCreateView(CreateView):
     model = Curso
     template_name = 'academi/curso_form.html'
@@ -32,7 +36,7 @@ class CursoCreateView(CreateView):
         form.instance.maestro = self.request.user.perfil_maestro
         return super().form_valid(form)
 
-
+#Editar cursos como maestro
 class CursoUpdateView(UpdateView):
     model = Curso
     template_name = 'academi/curso_update.html'
@@ -44,8 +48,71 @@ class CursoUpdateView(UpdateView):
         return super().form_valid(form)
 
 
+#Borrar cursos como maestro
 class CursoDeleteView(DeleteView):
     model = Curso
     template_name = 'academi/curso_confirm_delete.html'
     success_url = reverse_lazy('curso_list')
+
+
+#lista de cursos para inscribirse como estudiante
+class ListaCursoEstudianteView(ListView):
+    model = Curso
+    template_name = 'academi/listacursoEstudianteInscripcion.html'
+    context_object_name = 'cursos'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        try:
+            estudiante = self.request.user.perfil_estudiante
+            context['cursos_inscritos_ids'] = list(
+                Inscripcion.objects.filter(estudiante=estudiante)
+                .values_list('curso_id', flat=True)
+            )
+        except Estudiante.DoesNotExist:
+            context['cursos_inscritos_ids'] = []
+
+        return context
+
+class MisCursosEstudianteView(ListView):
+    model = Curso
+    template_name = 'academi/mis_cursos_estudiante.html'  
+    context_object_name = 'cursos'
+
+    def get_queryset(self):
+        try:
+            estudiante = self.request.user.perfil_estudiante
+        except Estudiante.DoesNotExist:
+            return Curso.objects.none()
+
+        return Curso.objects.filter(estudiantes_inscritos__estudiante=estudiante) 
+
+#Inscribirse al presionarm el boton logica
+class InscribirCursoView(View):
+        def post(self, request, id_curso):
+            curso_solicitado = get_object_or_404(Curso, id=id_curso)
+            try:
+                estudiante_actual = request.user.perfil_estudiante
+            except Estudiante.DoesNotExist:
+                return JsonResponse(
+                    {'mensaje': 'Solo los estudiantes pueden inscribirse a un curso.'},
+                    status=403
+                )
+            
+            inscripcion, creada = Inscripcion.objects.get_or_create(
+                estudiante = estudiante_actual,
+                curso = curso_solicitado
+            )
+
+            if creada:
+                mensaje = (
+                    f'¡Felicidades! Ya eres parte del curso "{curso_solicitado.nombre}" '
+                    f'con el profesor {curso_solicitado.maestro}'
+            )
+            else:
+                mensaje = 'Ya estabas inscrito en este curso.'
+
+            return JsonResponse({'mensaje': mensaje, 'creada': creada})
+
 
