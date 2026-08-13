da9597a1fbdf2ba0b1f7e1d1b94fb12970e1b89b
Carlos <crtorrescode@gmail.com>
2026-08-13 10:23:33 -0400

    feat: envio de tareas de parte del estudiante, adicion de multiples archivos a una tarea para la entrega

--- academico/views.py
+++ academico/views.py
@@ -1,8 +1,10 @@
+from django.utils import timezone
+
 from django.contrib import messages
-from django.http import JsonResponse, request
-from django.shortcuts import render
+from django.db.models import Exists, OuterRef
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
@@ -10,24 +12,26 @@ from django.views.generic import CreateView, DeleteView, DetailView, ListView, U
 
 from academico import models
 from academico.models import Curso, Inscripcion
-from evaluaciones.models import Tarea
+from evaluaciones.models import Calificacion, Tarea, ArchivoEntrega
 from usuarios.models import Estudiante
 
 # Create your views here.
 
-#Gestionar los cursos como maestro
+# Gestionar los cursos como maestro
 class CursoListView(ListView):
     model = Curso
     template_name = 'academi/curso_list.html'
     context_object_name = 'cursos'
 
-#detalles de los cursos como maestro
+
+# detalles de los cursos como maestro
 class CursoDetailView(DetailView):
     model = Curso
     template_name = 'academi/curso_detail.html'
     context_object_name = 'curso'
 
-#creación de cursos como maestro
+
+# creación de cursos como maestro
 class CursoCreateView(CreateView):
     model = Curso
     template_name = 'academi/curso_form.html'
@@ -38,7 +42,8 @@ class CursoCreateView(CreateView):
         form.instance.maestro = self.request.user.perfil_maestro
         return super().form_valid(form)
 
-#Editar cursos como maestro
+
+# Editar cursos como maestro
 class CursoUpdateView(UpdateView):
     model = Curso
     template_name = 'academi/curso_update.html'
@@ -50,14 +55,14 @@ class CursoUpdateView(UpdateView):
         return super().form_valid(form)
 
 
-#Borrar cursos como maestro
+# Borrar cursos como maestro
 class CursoDeleteView(DeleteView):
     model = Curso
     template_name = 'academi/curso_confirm_delete.html'
     success_url = reverse_lazy('curso_list')
 
 
-#lista de cursos para inscribirse como estudiante
+# lista de cursos para inscribirse como estudiante
 class ListaCursoEstudianteView(ListView):
     model = Curso
     template_name = 'academi/listacursoEstudianteInscripcion.html'
@@ -65,7 +70,6 @@ class ListaCursoEstudianteView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         try:
             estudiante = self.request.user.perfil_estudiante
             context['cursos_inscritos_ids'] = list(
@@ -74,12 +78,12 @@ class ListaCursoEstudianteView(ListView):
             )
         except Estudiante.DoesNotExist:
             context['cursos_inscritos_ids'] = []
-
         return context
+
 
 class MisCursosEstudianteView(ListView):
     model = Curso
-    template_name = 'academi/mis_cursos_estudiante.html'  
+    template_name = 'academi/mis_cursos_estudiante.html'
     context_object_name = 'cursos'
 
     def get_queryset(self):
@@ -88,36 +92,38 @@ class MisCursosEstudianteView(ListView):
         except Estudiante.DoesNotExist:
             return Curso.objects.none()
 
-        return Curso.objects.filter(estudiantes_inscritos__estudiante=estudiante) 
+        return Curso.objects.filter(estudiantes_inscritos__estudiante=estudiante)
 
-#Inscribirse al presionarm el boton logica
+
+# Inscribirse al presionar el boton, lógica
 class InscribirCursoView(View):
-        def post(self, request, id_curso):
-            curso_solicitado = get_object_or_404(Curso, id=id_curso)
-            try:
-                estudiante_actual = request.user.perfil_estudiante
-            except Estudiante.DoesNotExist:
-                return JsonResponse(
-                    {'mensaje': 'Solo los estudiantes pueden inscribirse a un curso.'},
-                    status=403
-                )
-            
-            inscripcion, creada = Inscripcion.objects.get_or_create(
-                estudiante = estudiante_actual,
-                curso = curso_solicitado
+    def post(self, request, id_curso):
+        curso_solicitado = get_object_or_404(Curso, id=id_curso)
+        try:
+            estudiante_actual = request.user.perfil_estudiante
+        except Estudiante.DoesNotExist:
+            return JsonResponse(
+                {'mensaje': 'Solo los estudiantes pueden inscribirse a un curso.'},
+                status=403
             )
 
-            if creada:
-                mensaje = (
-                    f'¡Felicidades! Ya eres parte del curso "{curso_solicitado.nombre}" '
-                    f'con el profesor {curso_solicitado.maestro}'
+        inscripcion, creada = Inscripcion.objects.get_or_create(
+            estudiante=estudiante_actual,
+            curso=curso_solicitado
+        )
+
+        if creada:
+            mensaje = (
+                f'¡Felicidades! Ya eres parte del curso "{curso_solicitado.nombre}" '
+                f'con el profesor {curso_solicitado.maestro}'
             )
-            else:
-                mensaje = 'Ya estabas inscrito en este curso.'
+        else:
+            mensaje = 'Ya estabas inscrito en este curso.'
 
-            return JsonResponse({'mensaje': mensaje, 'creada': creada})
+        return JsonResponse({'mensaje': mensaje, 'creada': creada})
 
-class tableroEstudianteView(LoginRequiredMixin,ListView):
+
+class tableroEstudianteView(LoginRequiredMixin, ListView):
     model = Tarea
     template_name = 'academi/tablero_estudiante.html'
     context_object_name = 'tareas'
@@ -128,14 +134,28 @@ class tableroEstudianteView(LoginRequiredMixin,ListView):
         except Estudiante.DoesNotExist:
             return Tarea.objects.none()
 
+        entregas = Calificacion.objects.filter(
+            tarea=OuterRef('pk'),
+            estudiante=estudiante
+        )
+
         return Tarea.objects.filter(
             curso__estudiantes_inscritos__estudiante=estudiante
-        ).select_related('curso').order_by('Fecha_entrega').distinct()
+        ).select_related('curso').annotate(
+            esta_entregada=Exists(entregas)
+        ).order_by('Fecha_entrega').distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ahora'] = timezone.now()
+        return context
+
 
 class DetalleTareaEstudianteView(LoginRequiredMixin, DetailView):
     model = Tarea
     template_name = 'academi/detalle_tarea_estudiante.html'
     context_object_name = 'tarea'
+    pk_url_kwarg = 'pk'
 
     def get_queryset(self):
         try:
@@ -145,6 +165,58 @@ class DetalleTareaEstudianteView(LoginRequiredMixin, DetailView):
 
         return Tarea.objects.filter(
             curso__estudiantes_inscritos__estudiante=estudiante
-        ).select_related('curso').distinct()    
+        ).select_related('curso').distinct()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            estudiante = self.request.user.perfil_estudiante
+            entrega = Calificacion.objects.filter(
+                tarea=self.object, estudiante=estudiante
+            ).first()
 
+            if entrega and (entrega.ruta_archivo or entrega.archivos_adjuntos.exists()):
+                context['ruta_archivo'] = True
+                context['puntuacion'] = entrega.puntuacion
+            else:
+                context['ruta_archivo'] = False
+                context['puntuacion'] = None
+
+            context['entrega'] = entrega
+        except Estudiante.DoesNotExist:
+            context['ruta_archivo'] = False
+            context['puntuacion'] = None
+            context['entrega'] = None
+
+        context['ahora'] = timezone.now()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = tarea_actual = self.get_object()
+
+        if tarea_actual.Fecha_entrega and tarea_actual.Fecha_entrega < timezone.now():
+            messages.error(request, "El plazo para entregar esta tarea ha expirado.")
+            return redirect('detalle_tarea_estudiante', pk=tarea_actual.pk)
+
+        try:
+            estudiante_actual = request.user.perfil_estudiante
+        except Estudiante.DoesNotExist:
+            messages.error(request, "Solo los estudiantes registrados pueden entregar tareas.")
+            return redirect('detalle_tarea_estudiante', pk=tarea_actual.pk)
+
+        archivos_subidos = request.FILES.getlist('archivos')
+
+        if archivos_subidos:
+            entrega, created = Calificacion.objects.get_or_create(
+                tarea=tarea_actual,
+                estudiante=estudiante_actual,
+            )
+
+            for f in archivos_subidos:
+                ArchivoEntrega.objects.create(entrega=entrega, archivo=f)
+
+            messages.success(request, f"¡Se entregaron {len(archivos_subidos)} archivos con éxito!")
+        else:
+            messages.error(request, "Hubo un problema. Asegúrate de adjuntar al menos un archivo antes de enviar.")
+
+        return redirect('detalle_tarea_estudiante', pk=tarea_actual.pk)
--- evaluaciones/models.py
+++ evaluaciones/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from pytz import timezone
+from django.utils import timezone  
 from tinymce.models import HTMLField
 from academico.models import Curso
+from usuarios.models import Estudiante
 
-# Create your models here.
+
 class Tarea(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = HTMLField()
@@ -18,12 +19,35 @@ class Tarea(models.Model):
         return 'Activa'
 
     def __str__(self):
-        return self.titulo    
+        return self.titulo
+
 
 class ArchivoTarea(models.Model):
     tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE, related_name='archivos')
     archivo = models.FileField(upload_to='archivos_tareas/')
 
     def __str__(self):
-        return f"Archivo para: {self.tarea.titulo}"
+        return f"Archivo de tarea: {self.tarea.titulo}"
 
+
+class Calificacion(models.Model):
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE, related_name='entregas')
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, related_name='calificaciones')
+    ruta_archivo = models.FileField(upload_to='entregas_estudiantes/', null=True, blank=True)
+    fecha_envio = models.DateTimeField(auto_now_add=True)
+    puntuacion = models.IntegerField(null=True, blank=True)
+    retroalimentacion = models.TextField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('tarea', 'estudiante')
+
+    def __str__(self):
+        return f"Entrega de {self.estudiante.usuario.username} - {self.tarea.titulo}"
+
+
+class ArchivoEntrega(models.Model):
+    entrega = models.ForeignKey(Calificacion, on_delete=models.CASCADE, related_name='archivos_adjuntos')
+    archivo = models.FileField(upload_to='entregas_estudiantes/multiples/')
+
+    def __str__(self):
+        return f"Adjunto de {self.entrega.estudiante.usuario.username} - {self.entrega.tarea.titulo}"
