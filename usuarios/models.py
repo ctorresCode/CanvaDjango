b44479c24f9da9c19c6bdbcaea90ceb1b3d8cb9e
Carlos <crtorrescode@gmail.com>
2026-08-22 11:27:44 -0400

    feat: modificación del modelo de estudiante para agregar el campo de foto del maestro

--- usuarios/models.py
+++ usuarios/models.py
@@ -33,6 +33,7 @@ class Maestro(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True, related_name='perfil_maestro')
     edad = models.PositiveIntegerField(null=True, blank=True)
     informacion_profesional = models.TextField(blank=True)
+    imagen_perfil = models.ImageField(upload_to='maestros/', null=True, blank=True)
 
     def __str__(self):
         return f"Maestro: {self.usuario.get_full_name() or self.usuario.username}" 
